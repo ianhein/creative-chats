69bd522a16b2b089185c079a6027775b9d6b0d33
ianhein <ianhein50@gmail.com>
2024-01-08 12:43:15 -0300

    Fix import casing and update Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,11 +2,25 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { UseState, UseEffect } from "react";
+import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import { Logo } from "../public/assets/images/logo.svg";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, getProviders] = useState(null);
+  const [toggleDropwdown, setToggleDropwdown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -17,7 +31,104 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
+        <p className="logo_text">CreativeChats</p>
       </Link>
+
+      {/* Navegación escritorio*/}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((providers) => (
+                <button
+                  type="button"
+                  key="provider.name"
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Navegación móvil*/}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropwdown((prev) => !prev)}
+            />
+
+            {toggleDropwdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropwdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropwdown(false)}
+                >
+                  Create prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropwdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((providers) => (
+                <button
+                  type="button"
+                  key="provider.name"
+                  onClick={() => {}}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
