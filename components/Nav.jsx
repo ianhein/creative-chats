36a23245b64e2284661877716ce7982beedcdb96
ianhein <ianhein50@gmail.com>
2024-01-09 00:32:15 -0300

    Add MongoDB connection and session handling

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,19 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  const [providers, getProviders] = useState(null);
-  const [toggleDropwdown, setToggleDropwdown] = useState(false);
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -36,7 +36,7 @@ const Nav = () => {
 
       {/* Navegación escritorio*/}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -46,7 +46,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -61,7 +61,7 @@ const Nav = () => {
                 <button
                   type="button"
                   key="provider.name"
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => signIn(providers.id)}
                   className="black_btn"
                 >
                   Sign In
@@ -73,10 +73,10 @@ const Nav = () => {
 
       {/* Navegación móvil*/}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="assets/images/logo.svg"
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -84,49 +84,38 @@ const Nav = () => {
               onClick={() => setToggleDropwdown((prev) => !prev)}
             />
 
-            {toggleDropwdown && (
+            {toggleDropdown && (
               <div className="dropdown">
                 <Link
                   href="/profile"
                   className="dropdown_link"
-                  onClick={() => setToggleDropwdown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
                   href="/create-prompt"
                   className="dropdown_link"
-                  onClick={() => setToggleDropwdown(false)}
+                  onClick={() => setToggleDropdown(false)}
                 >
-                  Create prompt
+                  Create Prompt
                 </Link>
                 <button
                   type="button"
                   onClick={() => {
-                    setToggleDropwdown(false);
+                    setToggleDropdown(false);
                     signOut();
                   }}
                   className="mt-5 w-full black_btn"
                 >
+                  {" "}
                   Sign Out
                 </button>
               </div>
             )}
           </div>
         ) : (
-          <>
-            {providers &&
-              Object.values(providers).map((providers) => (
-                <button
-                  type="button"
-                  key="provider.name"
-                  onClick={() => {}}
-                  className="black_btn"
-                >
-                  Sign In
-                </button>
-              ))}
-          </>
+          <></>
         )}
       </div>
     </nav>
